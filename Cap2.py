c3f1353b574fa0fb6cf7867857e3322915430a2a
JuanMoreno013 <101610177+JuanMoreno013@users.noreply.github.com>
2022-03-14 22:52:47 -0600

    Capitulo 2 VARIABLES

---
+++ Cap2.py
@@ -0,0 +1,12 @@
+#CAPITULO 2,  VARIABLES
+
+men = "Hola, primer programa en Python ?" #Le asignamos a la variable men, un valor de tipo string
+
+val1= 13      #Inicializamos val1 con un dato númerico 
+val2= 56      #Inicializamos val2 con un dato númerico
+
+result= val1 + val2 #Realizamos la suma de val1 y val2, guardando el resultado en la variable result
+
+print("\n" , men)      #Imprimimos el mensaje de texto
+print(" \t El resultado es: ", result, "\n") #Imprimimos el resultado de la suma 
+
