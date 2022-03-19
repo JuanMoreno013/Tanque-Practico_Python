a00510e68139b9a4b2e059a2d8e4c32106bb1cad
JuanMoreno013 <101610177+JuanMoreno013@users.noreply.github.com>
2022-03-18 18:49:20 -0600

    CAPITULO OPERADORES

---
+++ Cap7.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python 
+# * coding: cp1252 *
+# * coding: 850 *
+# * coding: utf-8 *
+
+#Usamos las lineas anteriores para codificar, y así poder utilizar caracteres especiales
+
+#CAPITULO 7,  OPERADORES 1 "Suma, resta, multiplicación y división" 
+
+suma = 23 + 24 + 40 #Le asignamos a la variable suma, un valor de tipo entero, donde utilizaremos el operador suma
+resta = 23 - 24 - 80 #Le asignamos a la variable resta, un valor de tipo entero usando numeros y el operador resta 
+multi = 10 * 5 * 7 #Le asignamos a la variable multi, un valor de tipo entero usando numeros y el operador multiplicacion 
+division = 5000 / 230 / 2 #Le asignamos a la variable division, un valor de tipo entero usando numeros y el operador de division 
+
+operacion = 10 / 5 + 15 - 17  #Le asignamos a la variable un valor de tipo entero usando numeros y todos los operador para que el resultado sea 0
+
+print(" \n\t\nSuma: " , suma ,  "\nResta: " , resta , "\nMultiplicación: " , multi , "\nDivisión: " , division) #Imprimimos todas las variables
+print("\n\t\n Usando todos los operadores", operacion) #Imprimimos el resultado de la variable operación, dado el uso de todos los operadores
---
+++ Cap8.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+# * coding: cp1252 *
+# * coding: 850 *
+# * coding: utf-8 *
+
+#Usamos las lineas anteriores para codificar, y así poder utilizar caracteres especiales
+
+#CAPITULO 8,  OPERADORES 2 "EXPONENTES"
+
+var = 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 
+
+var1= 2 ** 10 #Le asignamos a nuestra variable el resultado de elevar 2 a la decima, dando como resultado 1024
+
+print(var1) #Imprimimos el resultado de la variable
