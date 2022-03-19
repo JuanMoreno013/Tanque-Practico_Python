fdaf90a504415a050b3c11298fe26be3ba074cf6
JuanMoreno013 <101610177+JuanMoreno013@users.noreply.github.com>
2022-03-18 18:52:25 -0600

    CAPITULO FLOATS

---
+++ Cap9.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+# * coding: cp1252 *
+# * coding: 850 *
+# * coding: utf-8 *
+
+#Usamos las lineas anteriores para codificar, y así poder utilizar caracteres especiales
+
+#CAPITULO 9,  floats y el método round()
+
+numero = round(17.567383292929200234, 5) #Le asignamos a nuestra variable el método round que sirve para acotar lso decimales de un numero
+                                         # La sintaxis de este método es Round(resultado, numero de decimales a acotar)
+
+print(numero) #Imprimos el numero acotado a 5 decimales 
+
